ab558012a5f4ece3a036be2226ea5e7d5b1f6391
Hudson Hancock <hudsonhancock@gmail.com>
2021-09-25 21:37:58 -0400

    Adds models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,45 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
+const Tag = require("./Tag");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      // allowNull defaults to true
+      allowNull: true,
+      references: {
+        model: Product,
+        key: "id",
+      },
+    },
+
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // allowNull defaults to true
+      allowNull: true,
+      references: {
+        model: Tag,
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
